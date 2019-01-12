87addccb678e6205db453838367fca1f6a99c768
saifsust <saiful.sust.cse@gmail.com>
2019-01-12 11:13:11 +0600

    Excel Writer add Code refactory is done for effecient use

--- src/assignmentCheckerAlgorithm/pathString.java
+++
@@ -1,5 +0,0 @@
-package assignmentCheckerAlgorithm;
-interface pathString {
-	public static final String in = "K:/Tutorials/src/assignmentCheckerAlgorithm/input.txt";
-	public static final String out= "K:/Tutorials/src/assignmentCheckerAlgorithm/output.txt";
-}
